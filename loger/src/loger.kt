02a944436fa19f80642abe5c9d88480934f1d1e8
sergeychaliy2 <70662544+sergeychaliy2@users.noreply.github.com>
2021-10-16 17:56:42 +0300

    Update loger.kt

--- loger/src/loger.kt
+++ loger/src/loger.kt
@@ -1,12 +1,12 @@
 import java.io.File
 import java.io.PrintWriter
 
-interface:Interface{
+interface MyInterface{
     fun makeLoger(message: String)
 }
 Class Product{
      companion object{
-    fun getLoger(loger: Int):Interface{
+    fun getLoger(loger: Int):MyInterface{
         if(loger<=3){
             val prod=FLog()
             return prod
@@ -19,12 +19,12 @@ Class Product{
   }
 }
 
-class FLog: Interface{
+class FLog: MyInterface{
     override fun makeLoger(message: String){
         File("log.txt").appendText("")
     }
 }
 
-class SLog:Interface{
+class SLog:MyInterface{
     override fun makeLoger(message: String){
        println(message)
