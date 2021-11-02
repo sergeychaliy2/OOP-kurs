3bd7edbb03f801631b0732333ff5c59af32fce04
sergeychaliy2 <70662544+sergeychaliy2@users.noreply.github.com>
2021-11-02 10:01:26 +0300

    Update Main.kt

--- magazin/src/main/kotlin/Main.kt
+++ magazin/src/main/kotlin/Main.kt
@@ -1,15 +1,10 @@
-fun main() {
-    val magazins = Katalog("Verniy")
-    val drink:Products = Drink(5)
+fun main(args: Array<String>) {
+    val shop = Catalog("loyal")
+    val drink:Products = Drink(5, 0.2F,0.3F)
     val eat:Products = Eat(19)
-    val candies:Products = Candies(20)
-    magazins.plusProduct(drink)
-    magazins.plusProduct(eat)
-    magazins.plusProduct(candies)
-    magazins.KolVo()
-    magazins.minusProduct(candies)
-    magazins.KolVo()
-    println(eat.price)
-    println(drink.price)
-
+    Candies(20)
+    shop.plusProduct(drink)
+    shop.plusProduct(eat)
+    shop.Number()
+    drink.alhog()
 }
