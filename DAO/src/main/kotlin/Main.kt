f29dfbe06040625b4573780a22086245b79c11f0
sergeychaliy2 <70662544+sergeychaliy2@users.noreply.github.com>
2021-11-02 09:58:22 +0300

    Update Main.kt

--- DAO/src/main/kotlin/Main.kt
+++ DAO/src/main/kotlin/Main.kt
@@ -1,11 +1,10 @@
 fun main() {
-    val apple: Products = Apple("Apple", "red")
-    val banana: Products = Banana("Banana", "yelow")
-val list= mutableListOf<String>()
+    val apple: Products = Apple( _info = "green")
+    val banana: Products = Banana(_info="yellow")
     val obj = DaoProducts()
 obj.writeProduct(apple)
     obj.writeProduct(banana)
-    obj.readProduct(list)
-    println(list)
+    obj.readProduct()
+
 
 }
