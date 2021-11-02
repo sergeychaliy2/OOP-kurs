3296269d465173051da2cf17957afb6fcc36d055
sergeychaliy2 <70662544+sergeychaliy2@users.noreply.github.com>
2021-11-02 09:56:53 +0300

    Update Product.kt

--- DAO/src/main/kotlin/Product.kt
+++ DAO/src/main/kotlin/Product.kt
@@ -1,29 +1,28 @@
 import java.io.File
 import java.io.InputStream
 abstract class Products{
+    abstract val _info: String
     abstract val name: String
-    abstract val info: String
 }
-data class Apple(override val name: String, override val info: String): Products()
-data class Banana(override val name: String, override val info: String): Products()
+data class Apple(override val name: String ="Apple", override val _info: String): Products()
+data class Banana(override val name: String ="Banana", override val _info: String): Products()
 
 interface Dao{
 
-    fun readProduct(list: MutableList<String>)
+    fun readProduct():MutableList<String>
     fun writeProduct(prod: Products)
 }
  class DaoProducts: Dao {
      private val path="text.txt"
-     override fun readProduct(list: MutableList<String>) {
+     private val list= mutableListOf<String>()
+     override fun readProduct(): MutableList<String> {
          if(File(path).exists()){
-             File(path).forEachLine  {
-                 list.add(it)
-             }
+            val obj= File(path).readText()
+          list.add(obj)
          }
+         return list
      }
-
      override fun writeProduct(prod: Products) {
-         File(path).bufferedWriter().use{out-> out.write("")}
          File(path).appendText("$prod\n")
      }
 
