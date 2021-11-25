d78606f829131dea4166e159253c5625635696ad
sergeychaliy2 <70662544+sergeychaliy2@users.noreply.github.com>
2021-11-25 20:50:26 +0300

    Delete model.kt

--- model.kt
+++
@@ -1,9 +0,0 @@
-class Model{
-    private val list= mutableListOf("Alex","Sergey")
-    fun getFirstElement():String{
-        return list.elementAt(list.size-1)
-    }
-    fun getMutableList():MutableList<String>{
-        return list
-    }
-}
